49c74e0b4ca88698b6832ccd246e416e2fde1b91
huanglei <huanglei@yegoo.com>
2018-03-27 18:27:09 +0800

    重构代码 使用electron作为ui

---
+++ jd/jd.go
@@ -0,0 +1,287 @@
+package jd
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+const (
+	// QRURL .
+	QRURL = "https://qr.m.jd.com/show?appid=133&size=147"
+	// CheckURL .
+	CheckURL = "https://qr.m.jd.com/check?callback=callback&appid=133"
+	// AuthURL .
+	AuthURL = "https://passport.jd.com/uc/qrCodeTicketValidation"
+	// TryURL .
+	TryURL = "http://try.jd.com/migrate/apply?source=0&activityId=253484"
+	// TryProductURL .
+	TryProductURL = "https://try.jd.com/activity/getActivityList"
+	// Referer .
+	Referer = "https://passport.jd.com/new/login.aspx"
+
+	// UserURL .
+	UserURL = "https://i.jd.com/user/info"
+)
+
+type jdCookie struct {
+	cookies []*http.Cookie
+}
+
+func (cookie *jdCookie) getCookie(key string) (*http.Cookie, error) {
+	for _, cookie := range cookie.cookies {
+		if cookie.Name == key {
+			return cookie, nil
+		}
+	}
+	return nil, errors.New("cookie not found")
+}
+
+// Channel 通道
+type Channel struct {
+	Cmd  int16       `json:"cmd"`
+	Data interface{} `json:"data"`
+}
+
+// Product 商品
+type Product struct {
+	AID   string `json:"id"`
+	Name  string `json:"name"`
+	Price string `json:"price"`
+	Img   string `json:"img"`
+}
+
+// User 用户信息你
+type User struct {
+	Name   string `json:"name"`
+	Avatar string `json:"avatar"`
+}
+
+// Option 参数
+type Option struct {
+	Callback func(*Channel)
+}
+
+// JD ...
+type JD struct {
+	qrCookie   *jdCookie
+	thorCookie *http.Cookie
+	option     *Option
+	channal    chan Channel
+}
+
+// 获取商品
+func (jd *JD) getProducts() {
+	url := TryProductURL
+	resp, err := http.Get(url)
+	if err != nil {
+		jd.e(err)
+		return
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		jd.e(err)
+		return
+	}
+	resp.Body.Close()
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(string(body)))
+	if err != nil {
+		jd.e(err)
+		return
+	}
+
+	// 获取总共多少条数据
+	totalPage, err := strconv.Atoi(doc.Find(".page .p-skip b").Text())
+	if err != nil {
+		jd.e(err)
+		return
+	}
+
+	// 解析产品数据
+	parseProduct := func(doc *goquery.Document) {
+		var products = []Product{}
+		doc.Find("#goods-list .items .item").Each(func(i int, s *goquery.Selection) {
+			aID, _ := s.Attr("activity_id")
+			name := s.Find(".p-name").Text()
+			price := s.Find(".p-price").Text()
+			img, _ := s.Find(".p-img img").Attr("src")
+			product := Product{AID: aID, Name: name, Price: price, Img: "http://" + img}
+			products = append(products, product)
+		})
+		jd.option.Callback(&Channel{Cmd: 20, Data: products})
+	}
+
+	// 第一页已经取出 直接使用
+	go parseProduct(doc)
+	// 从第二页开始循环
+	for i := 2; i <= totalPage; i++ {
+		productDoc, _ := goquery.NewDocument(url + "?page=" + strconv.Itoa(i))
+		go parseProduct(productDoc)
+	}
+}
+
+// 获取cookies
+func (jd *JD) getCookie(ticket string) {
+	url := AuthURL + "?t=" + ticket
+	resp, err := http.Get(url)
+	if err != nil {
+		jd.e(err)
+		return
+	}
+	jd.thorCookie, _ = (&jdCookie{resp.Cookies()}).getCookie("thor")
+	jd.getUser()
+}
+
+// 获取登录二维码
+func (jd *JD) getQRImage() {
+	log.Println("获取登录二维码")
+	url := QRURL + "&t=" + jd.getTimestamp()
+	resp, err := http.Get(url)
+	if err != nil {
+		jd.e(err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		jd.e(err)
+		return
+	}
+	jd.qrCookie = &jdCookie{resp.Cookies()}
+	image := "data:image/png;base64," + base64.StdEncoding.EncodeToString(body)
+	jd.option.Callback(&Channel{Cmd: 10, Data: image})
+}
+
+// 获取用户信息
+func (jd *JD) getUser() {
+	req, err := http.NewRequest("GET", UserURL, nil)
+	if err != nil {
+		jd.e(err)
+		return
+	}
+	req.AddCookie(jd.thorCookie)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		jd.e(err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		jd.e(err)
+		return
+	}
+	doc, _ := goquery.NewDocumentFromReader(strings.NewReader(string(body)))
+	name := doc.Find("#user-info .info-m B").Text()
+	avatar, _ := doc.Find("#user-info .u-pic img").Attr("src")
+	jd.option.Callback(&Channel{Cmd: 30, Data: User{Name: name, Avatar: "http://" + avatar}})
+}
+
+// 检查是否登录
+func (jd *JD) onCheck() {
+	type CheckResult struct {
+		Code   int    `json:"code"`
+		Msg    string `json:"msg"`
+		Ticket string `json:"ticket"`
+	}
+	for {
+		time.Sleep(3000 * time.Millisecond)
+		if jd.qrCookie == nil {
+			continue
+		}
+		qrCookie, err := jd.qrCookie.getCookie("QRCodeKey")
+		if err != nil {
+			continue
+		}
+		tokenCookie, err := jd.qrCookie.getCookie("wlfstk_smdl")
+		if err != nil {
+			continue
+		}
+		url := CheckURL + "&token=" + tokenCookie.Value + "&_=" + jd.getTimestamp()
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			jd.e(err)
+			continue
+		}
+		// 设置cookies
+		req.AddCookie(&http.Cookie{Name: "QRCodeKey", Value: qrCookie.Value})
+		req.AddCookie(&http.Cookie{Name: "wlfstk_smdl", Value: tokenCookie.Value})
+		req.Header.Add("Referer", Referer)
+		// 发送请求
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			jd.e(err)
+			continue
+		}
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			jd.e(err)
+			continue
+		}
+		resp.Body.Close()
+		str := strings.Replace(strings.Replace(string(body), "callback({", "{", 1), "})", "}", 1)
+		var result CheckResult
+		if err := json.Unmarshal([]byte(str), &result); err != nil {
+			jd.e(err)
+			continue
+		}
+		if result.Code == 200 {
+			jd.getCookie(result.Ticket)
+			break
+		}
+		if result.Code == 205 || result.Code == 203 {
+			jd.qrCookie = nil
+			jd.option.Callback(&Channel{Cmd: 11, Data: ""})
+			continue
+		}
+	}
+}
+
+// Send 发送消息
+func (jd *JD) Send(c *Channel) {
+	jd.channal <- *c
+}
+
+// 通道获取数据
+func (jd *JD) onChannel() {
+	for {
+		send := <-jd.channal
+		switch send.Cmd {
+		case 1:
+			go jd.getQRImage()
+			go jd.onCheck()
+		case 2:
+			go jd.getProducts()
+		}
+	}
+}
+
+// 当前时间戳
+func (jd *JD) getTimestamp() string {
+	return strconv.FormatInt(time.Now().Unix(), 10)
+}
+
+func (jd *JD) e(err error) {
+	log.Fatalln(err)
+	jd.option.Callback(&Channel{Cmd: -100, Data: err.Error()})
+}
+
+// New 创建实例
+func New(option *Option) (*JD, error) {
+	if option == nil || option.Callback == nil {
+		return nil, errors.New("参数不能为空")
+	}
+	jd := JD{option: option, channal: make(chan Channel)}
+	go jd.onChannel()
+	return &jd, nil
+}
--- jdautotry.go
+++ jdautotry.go
@@ -1,309 +1,90 @@
 package main
 
 import (
-	"bytes"
-	"encoding/base64"
 	"encoding/json"
-	"errors"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"log"
-	"net/http"
-	"os"
-	"strconv"
-	"strings"
-	"time"
 
-	"github.com/PuerkitoBio/goquery"
-	sciter "github.com/sciter-sdk/go-sciter"
-	"github.com/sciter-sdk/go-sciter/window"
+	"github.com/asticode/go-astilectron"
+	bootstrap "github.com/asticode/go-astilectron-bootstrap"
+	"github.com/ihuanglei/jdautotry/jd"
+	"github.com/pkg/errors"
 )
 
-const (
-	// QRURL .
-	QRURL = "https://qr.m.jd.com/show?appid=133&size=147"
-	// CheckURL .
-	CheckURL = "https://qr.m.jd.com/check?callback=callback&appid=133"
-	// AuthURL .
-	AuthURL = "https://passport.jd.com/uc/qrCodeTicketValidation"
-	// TryURL .
-	TryURL = "http://try.jd.com/migrate/apply?source=0&activityId=253484"
-	// TryProductURL .
-	TryProductURL = "https://try.jd.com/activity/getActivityList"
-	// Referer .
-	Referer = "https://passport.jd.com/new/login.aspx"
-
-	// UserURL .
-	UserURL = "https://i.jd.com/user/info"
+// Vars
+var (
+	AppName string
+	BuiltAt string
+	w       *astilectron.Window
+	j       *jd.JD
 )
-
-// JDCookie .
-type JDCookie struct {
-	cookies []*http.Cookie
-}
-
-func (jdCookie *JDCookie) getCookie(key string) (*http.Cookie, error) {
-	for _, cookie := range jdCookie.cookies {
-		if cookie.Name == key {
-			return cookie, nil
-		}
-	}
-	return nil, errors.New("cookie not found")
-}
-
-// CheckResult .
-type CheckResult struct {
-	Code   int    `json:"code"`
-	Msg    string `json:"msg"`
-	Ticket string `json:"ticket"`
-}
-
-// TryResult .
-type TryResult struct {
-	Code    string `json:"code"`
-	Msg     string `json:"message"`
-	Success bool   `json:"success"`
-}
-
-var globalJdQRCookie *JDCookie
-var globalThorCookie *http.Cookie
-var globalCallbackChan = make(chan string)
-var uiLog *sciter.Element
 
 func main() {
-	go check()
-	w, err := window.New(sciter.SW_TITLEBAR|sciter.SW_CONTROLS|sciter.SW_MAIN, &sciter.Rect{Left: 0, Top: 0, Right: 541, Bottom: 548})
-	if err != nil {
-		panic(err)
-	}
-	w.LoadFile("index.html")
-	w.SetTitle("京东试用")
-	initUI(w)
-	initFunc(w)
-	initCallback(w)
-	w.Show()
-	w.Run()
+	initJD()
+	initUI()
 }
 
-// 初始化页面
-func initUI(w *window.Window) {
-	root, err := w.GetRootElement()
+func initJD() {
+	jd, err := jd.New(&jd.Option{Callback: jdCallback})
 	if err != nil {
-		panic(err)
+		fmt.Println(err.Error())
 	}
-	el, err := root.SelectById("ui-log")
-	if err != nil {
-		panic(err)
-	}
-	uiLog = el
+	j = jd
 }
 
-//定义函数
-func initFunc(w *window.Window) {
-	//定义reloadQR重新获取
-	w.DefineFunction("getQRImage", func(args ...*sciter.Value) *sciter.Value {
-		data, err := getQRImage()
-		if err != nil {
-			allPrintLn(err.Error())
-			return sciter.NullValue()
-		}
-		return sciter.NewValue("data:image/png;base64," + data)
-	})
-}
-
-// 定义回调
-func initCallback(w *window.Window) {
-	cb := &sciter.CallbackHandler{
-		//加载数据过程中
-		OnDataLoaded: func(p *sciter.ScnDataLoaded) int {
-			// fmt.Println("加载中:", p.Uri())
-			return sciter.LOAD_OK
+func initUI() {
+	if err := bootstrap.Run(bootstrap.Options{
+		AstilectronOptions: astilectron.Options{
+			AppIconDarwinPath:  "resources/icon.icns",
+			AppIconDefaultPath: "resources/icon.png",
 		},
+		Debug:    true,
+		Homepage: "index.html",
+		MenuOptions: []*astilectron.MenuItemOptions{{
+			Label: astilectron.PtrStr("File"),
+			SubMenu: []*astilectron.MenuItemOptions{
+				{Label: astilectron.PtrStr("About")},
+				{Role: astilectron.MenuItemRoleClose},
+			},
+		}},
+		OnWait: func(_ *astilectron.Astilectron, iw *astilectron.Window, _ *astilectron.Menu, _ *astilectron.Tray, _ *astilectron.Menu) error {
+			w = iw
+			w.OnMessage(jsCallback)
+			return nil
+		},
+		WindowOptions: &astilectron.WindowOptions{
+			BackgroundColor: astilectron.PtrStr("#fff"),
+			Center:          astilectron.PtrBool(true),
+			Height:          astilectron.PtrInt(700),
+			Width:           astilectron.PtrInt(700),
+		},
+	}); err != nil {
+		fmt.Println(errors.Wrap(err, "running bootstrap failed"))
 	}
-	w.SetCallback(cb)
-	go func() {
-		for {
-			val := <-globalCallbackChan
-			w.Call("callback", sciter.NewValue(val))
-		}
-	}()
-
-	globalCallbackChan <- "init"
 }
 
-// 页面显示日志
-func uiPrintLn(message string) {
-	uiLog.SetHtml("<p>"+message+"</p>", sciter.SIH_INSERT_AT_START)
+// 页面回调
+func jsCallback(m *astilectron.EventMessage) interface{} {
+	var s string
+	m.Unmarshal(&s)
+	fmt.Println(s)
+	if s == "getQRImg" {
+		j.Send(&jd.Channel{Cmd: 1})
+	} else if s == "getProduct" {
+		j.Send(&jd.Channel{Cmd: 2})
+	}
+	return nil
 }
 
-// 控制台及页面显示日志
-func allPrintLn(message string) {
-	log.Println(message)
-	uiPrintLn(message)
-}
-
-// 登录跳转及获取cookies
-func login(ticket string) {
-	url := AuthURL + "?t=" + ticket
-	fmt.Println(url)
-	resp, err := http.Get(url)
+// 京东回调
+func jdCallback(c *jd.Channel) {
+	bs, err := json.Marshal(c)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		return
 	}
-	globalCallbackChan <- "qr-login-ok"
-	cookie, _ := (&JDCookie{resp.Cookies()}).getCookie("thor")
-	getUser(cookie)
-}
-
-// 试用
-func tryIt() {
-	url := TryURL
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		panic(err)
-	}
-
-	req.AddCookie(globalThorCookie)
-	req.Header.Add("Referer", Referer)
-
-	log.Println(req)
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-	log.Println(resp)
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	var result TryResult
-	if err := json.Unmarshal(body, &result); err != nil {
-		panic(err)
-	}
-	fmt.Println(result)
-}
-
-// 获取登录二维码
-func getQRImage() (string, error) {
-	log.Println("获取登录二维码")
-	url := QRURL + "&t=" + getTimestamp()
-	resp, err := http.Get(url)
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-	globalJdQRCookie = &JDCookie{resp.Cookies()}
-	return base64.StdEncoding.EncodeToString(body), nil
-}
-
-// 检查是否登录
-func check() {
-	var lastCode int
-	for {
-		log.Println("检查登录")
-		time.Sleep(3000 * time.Millisecond)
-		if globalJdQRCookie == nil {
-			continue
-		}
-		qrCookie, err := globalJdQRCookie.getCookie("QRCodeKey")
-		if err != nil {
-			continue
-		}
-		tokenCookie, err := globalJdQRCookie.getCookie("wlfstk_smdl")
-		if err != nil {
-			continue
-		}
-		url := CheckURL + "&token=" + tokenCookie.Value + "&_=" + getTimestamp()
-		req, err := http.NewRequest("GET", url, nil)
-		if err != nil {
-			allPrintLn(err.Error())
-			continue
-		}
-		// 设置cookies
-		req.AddCookie(&http.Cookie{Name: "QRCodeKey", Value: qrCookie.Value})
-		req.AddCookie(&http.Cookie{Name: "wlfstk_smdl", Value: tokenCookie.Value})
-		req.Header.Add("Referer", Referer)
-
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			allPrintLn(err.Error())
-			continue
-		}
-		defer resp.Body.Close()
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			allPrintLn(err.Error())
-			continue
-		}
-		log.Println(string(body))
-		str := strings.Replace(strings.Replace(string(body), "callback({", "{", 1), "})", "}", 1)
-		var result CheckResult
-		if err := json.Unmarshal([]byte(str), &result); err != nil {
-			allPrintLn(err.Error())
-			continue
-		}
-
-		if lastCode != result.Code {
-			uiPrintLn(result.Msg)
-		}
-		if result.Code == 200 {
-			allPrintLn("二维码授权成功")
-			login(result.Ticket)
-			break
-		}
-		if result.Code == 205 || result.Code == 203 {
-			allPrintLn(result.Msg)
-			lastCode = 0
-			globalJdQRCookie = nil
-			globalCallbackChan <- "qr-timeout"
-			continue
-		}
-		lastCode = result.Code
-	}
-}
-
-func getUser(cookie *http.Cookie) {
-	req, err := http.NewRequest("GET", UserURL, nil)
-	if err != nil {
-		allPrintLn(err.Error())
-	}
-	req.AddCookie(cookie)
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		allPrintLn(err.Error())
-	}
-	defer resp.Body.Close()
-
-	body, _ := ioutil.ReadAll(resp.Body)
-	doc, _ := goquery.NewDocumentFromReader(strings.NewReader(string(body)))
-	name := doc.Find("#user-info .info-m B").Text()
-}
-
-// 当前时间戳
-func getTimestamp() string {
-	return strconv.FormatInt(time.Now().Unix(), 10)
-}
-
-func getProducts() {
-
-}
-
-func saveQRImage(b []byte) {
-	out, err := os.Create("jdqr.png")
-	if err != nil {
-		panic(err)
-	}
-	defer out.Close()
-	_, err = io.Copy(out, bytes.NewReader(b))
+	jsonStr := string(bs)
+	log.Println(jsonStr)
+	w.SendMessage(jsonStr, func(m *astilectron.EventMessage) {
+	})
 }
